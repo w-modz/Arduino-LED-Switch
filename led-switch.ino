d279b7791641af09e942db9a9e6b976f8869ed23
Adam Morawiec <amrw@pm.me>
2019-01-04 17:07:15 +0100

    Add debounce method for the switch flicker

--- led-switch.ino
+++ led-switch.ino
@@ -1,19 +1,32 @@
 int switchPin = 8;
 int ledPin = 13;
+bool lastButton = LOW;
+bool currentButton = LOW;
+bool ledOn = false;
+
+bool debounce(bool last) {
+  bool current = digitalRead(switchPin);
+
+  if (last != current) {
+    delay(5);
+    current = digitalRead(switchPin);
+  }
+
+  return current;
+}
 
 void setup() {
-  // put your setup code here, to run once:
   pinMode(switchPin, INPUT);
   pinMode(ledPin, OUTPUT);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-  if (digitalRead(switchPin) == HIGH) {
-    digitalWrite(ledPin, HIGH);
+  currentButton = debounce(lastButton);
+
+  if (lastButton == LOW && currentButton == HIGH) {
+    ledOn = !ledOn;
   }
-  else if (digitalRead(ledPin) == HIGH && digitalRead(switchPin) == HIGH) {
-    digitalWrite(ledPin, LOW);
-  }
-  
+
+  lastButton = currentButton;
+  digitalWrite(ledPin, ledOn);
 }
